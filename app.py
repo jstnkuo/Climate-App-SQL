5aedc815db771ca2c703f8713babb0c74721d78f
Justin Kuo <justinwkuo@gmail.com>
2023-02-23 17:04:21 -0800

    third push

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-session = Session(bind=engine)
 
 #flask set up
 app = Flask(__name__)
@@ -21,17 +20,26 @@ app = Flask(__name__)
 #flash route
 @app.route("/")
 def home():
-    return("hello")
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations<br/>"
+    )
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
+    # Create our session (link) from Python to the DB
+    session = Session(bind=engine)
+
     # Design a query to retrieve the last 12 months of precipitation data and plot the results. 
     # Starting from the most recent data point in the database. 
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
     # Calculate the date one year from the last date in data set.
     one_year = dt.date(2017,8,23) - dt.timedelta(days=365)
-
 
     # Perform a query to retrieve the data and precipitation scores
     sel = [Measurement.date, Measurement.prcp]
@@ -40,13 +48,40 @@ def prcp():
                               .filter(Measurement.date>=one_year)\
                               .all()
     session.close()
+
+    # Input result into a dictionary
     precip = {date: prcp for date, prcp in measurement_year}
     return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    stations = session.query(Station.station).all()    
-    return jsonify(stations)
+    # Create our session (link) from Python to the DB
+    session = Session(bind=engine)
+
+    # Query to select all the stations from dataset
+    stations = engine.execute('select station from station').fetchall()
+    session.close()
+
+    # Convert to normal list
+    all_stations= list(np.ravel(stations))
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(bind=engine)
+
+    #Query the dates and temperature observations of the most-active station 
+    #for the previous year of data
+    one_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    tobs_result = session.query(Measurement.tobs).filter(Measurement.date>=one_year).filter(Measurement.station == 'USC00519281').all()
+    session.close()
+
+    # Input result into a dictionary
+    tobs_year = list(np.ravel(tobs_result))
+    return jsonify(tobs_year)
+
 
 #run flask
 if __name__ == '__main__':
